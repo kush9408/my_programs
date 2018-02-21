0d8a6895365bf010d88f2cd27b799db7e7baafd3
kush sharma <kushsharma1998@gmail.com>
2018-02-21 19:33:22 +0530

    count number of characters in a given file

---
+++ count_charactersfh3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+char ch;
+int c=0;
+ifstream fin;
+fin.open("marks.dat",ios::in);
+while(fin)
+{
+fin.get(ch);
+c++;
+}
+cout<<"total number of characters in file are :"<<c;
+fin.close();
+return 0;
+}
+
