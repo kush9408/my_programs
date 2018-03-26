405df48ff48a7ffa66f8d07d3365d90f16d7638d
kush sharma <kushsharma1998@gmail.com>
2018-03-26 18:17:07 +0530

    updated code

--- inheritance_account.cpp
+++ inheritance_account.cpp
@@ -5,7 +5,7 @@ class Account
 private:
 char name[20];
 long ano;      //ano=account number
-long balance;   
+unsigned long balance;   
 public:
 void details()
 {
@@ -17,7 +17,15 @@ cout<<"Opening balance "<<endl;
 cin>>balance;
 cout<<endl;
 }
-void display()
+void display();
+
+long get_balance()
+{
+return balance; 
+}      
+}*p; //pointer for base class
+
+void Account::display()
 {
 cout<<"\t"<<"*****ACCOUNT*****"<<endl;
 cout<<endl<<endl;
@@ -25,48 +33,54 @@ cout<<"Account number:"<<ano<<endl;
 cout<<"Name:"<<name<<endl;
 cout<<"Opening balance:"<<balance<<endl;
 } 
-long get_balance()
-{
-return balance; 
-}      
-}a;
 class Current: public Account //CA=current account
 {
+long r;
 public:
-void deposit()
+void deposit();
+void withdraw();
+void check_minbalance();
+}c;
+
+void Current::deposit()
 {
-int amount,r=0;
+display(); 
+
+int amount;
 cout<<"Enter the amount to deposit in the account.....";
 cin>>amount;
-r =a.get_balance()+amount;
+r =p->get_balance();
+cout<<endl;
+cout<<r;
+cout<<endl;
+r=r+amount;
 cout<<"The new balance in the amount after the Deposit is: "<<r;
 }
-void withdraw()
+void Current:: withdraw()
 {
 int money;
 cout<<"How much money do you want to withdraw....."<<endl;
 cin>>money;
-if(money<a.get_balance())
+if(money<p->get_balance())
 {
 int balance;
 cout<<"Transaction in process:................"<<endl;
 cout<<"Money withdraw is :"<<money<<endl;
-balance=a.get_balance()-money;
+balance=p->get_balance()-money;
 cout<<"Balance amount in your acccount is: "<<balance<<endl;
 }
 else
 cout<<"You cannot withdraw money from the account"<<endl;
 }
-void check_minbalance()
+void Current:: check_minbalance()
 {
-if(a.get_balance()<500)
+if(p->get_balance()<500)
 {
-cout<<"The balance of the account should not be less than 500.....the penalty imposed is"<<a.get_balance()-500;
+cout<<"The balance of the account should not be less than 500.....the penalty imposed is"<<p->get_balance()-500;
 }
 else
-cout<<"Your Account Balance is:"<<a.get_balance();
+cout<<"Your Account Balance is:"<<p->get_balance();
 }
-}c;
 class Saving: public Account //SA= savings account
 {
 public:
@@ -75,7 +89,7 @@ void deposit()
 int amount,r;
 cout<<"\n Enter the amount to deposit in the account.....";
 cin>>amount;
-r =a.get_balance()+amount;
+r =p->get_balance()+amount;
 cout<<"\n The new balance in the amount after the Deposit is: "<<r;
 }
 void withdraw()
@@ -83,7 +97,7 @@ void withdraw()
 int money,r;
 cout<<"\n How much money do you want to withdraw....."<<endl;
 cin>>money;
-r=a.get_balance();
+r=p->get_balance();
 if(money<r)
 {
 cout<<"Transaction in process"<<endl;
@@ -97,7 +111,7 @@ cout<<"You cannot withdraw money from the account"<<endl;}
 void interest(int time,int rate)
 {
 int r;
-r=a.get_balance();
+r=p->get_balance();
 void display();
 int t=time;
 int ra=rate;
@@ -107,6 +121,7 @@ r=r+inter;
 cout<<"After calculating interest, balance is"<<r<<endl;
 }
 }s;
+
 int main()
 {
 char ch;
@@ -115,9 +130,9 @@ cout<<endl;
 cin>>ch;
 int choice,t,r;
 if(ch=='c'||ch=='C')
-{
-a.details();
-a.display();
+{p=&c;
+p->details();
+p->display();
 cout<<"\nDO YOU WANT TO:"<<endl;
 cout<<"1. Deposit"<<endl<<"2. Withdraw"<<endl<<"3. Check Balance"<<endl<<"4. Do nothing"<<endl;
 cout<<"Enter the choice from(1-4....)"<<endl;
@@ -144,13 +159,14 @@ cout<<"\n none of the option matched"<<endl;
 }
 }
 else if(ch=='s'||ch=='S')
-{
-a.details();
-a.display();
+{ p=&s;
+p->details();
+p->display();
 cout<<"\n do you want to :"<<endl;
 cout<<endl;
 cout<<"     1. Deposit"<<endl<<"     2. Withdraw"<<endl<<"     3. Find Interest"<<endl<<"     4. Do nothing"<<endl;;
 cout<<"Enter the choice from(1-4....)"<<endl;
+cin>>choice;
 switch(choice)
 {
 case 1: 
